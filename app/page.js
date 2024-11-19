71bc38221b40e7f177037cdc6276490e4b06d99c
mehul515 <mehulkohad@gmail.com>
2024-11-19 19:42:01 +0530

    Initial Commit

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2>Hello World!</h2>
+      <h2>Integrated Healthcare Platform</h2>
+      <h3>Final Year Project</h3>
     </div>
   );
 }
